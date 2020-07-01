044fd0a042845b9f320c163cf4bd80b21bc33b5c
Arthur Genet <arthurgenet@yahoo.fr>
2020-07-01 16:44:52 +0200

    test32

--- app/charts.js
+++ app/charts.js
@@ -1,16 +1,6 @@
-define(["app/config", "app/utils", "app/statistics", "app/main"], function (config, appUtils, statistics,main) {
+define(["app/config", "app/utils", "app/statistics"], function (config, appUtils, statistics) {
   Chart.defaults.global.defaultFontFamily = `"Avenir Next W00","Helvetica Neue",Helvetica,Arial,sans-serif`;
   Chart.defaults.global.defaultFontSize = 12;
-
-  var def_expression_date = "1=1 ";
-  var def_expression_height = "AND 1=1 ";
-  var def_expression_usage = "AND 1=1";
-
-  var click_year = false;
-  var click_height = false;
-  var click_usage = false;
-
-
   function createYearChart() {
     const yearCanvas = document.getElementById("yearChart");
     const yearChart = new Chart(yearCanvas.getContext("2d"), {
@@ -19,7 +9,7 @@ define(["app/config", "app/utils", "app/statistics", "app/main"], function (conf
         labels: config.yearClasses.map(function (element) { return element.label }),
         datasets: [
           {
-            label: "Gebouwde gebouwen",
+            label: "Buildings built",
             backgroundColor: config.yearClasses.map(function (element) { return element.color }),
             stack: "Stack 0",
             data: [0, 0, 0, 0, 0, 0]
@@ -33,7 +23,7 @@ define(["app/config", "app/utils", "app/statistics", "app/main"], function (conf
         },
         title: {
           display: true,
-          text: "Aantal gebouwen per bouwjaar"
+          text: "Number of buildings by construction year"
         },
         scales: {
           xAxes: [
@@ -53,50 +43,18 @@ define(["app/config", "app/utils", "app/statistics", "app/main"], function (conf
         }
       }
     });
-      yearCanvas.onclick = function(evt)
-
-    {   
-      if (click_year == false){
-
-        click_year = true;
-        var activePoints = yearChart.getElementsAtEvent(evt);
-        var clickedElementindex = activePoints[0]["_index"];
-        var label = yearChart.data.labels[clickedElementindex];
-        var dates = label.split(" ");
-        
-        
-        if (dates[2] != null) {
-          var start_date = dates[0];
-          var end_date = dates[2];
-          def_expression_date = "Bouwjaar >= " + start_date + " AND Bouwjaar < " + end_date + " ";
-        }
-        else {
-          var date = dates[0].substring(dates[0].lastIndexOf("<") + 1, dates[0].length);
-          def_expression_date = "Bouwjaar < " + date + " ";
-        }
-      }
-        
-      else{
-        click_year = false;
-        def_expression_date = "1=1 ";
-      }
-      
-
-      defExpression(def_expression_date,def_expression_height,def_expression_usage);  
-    }
-  
     return yearChart;
   }
   function createHeightChart() {
     const heightCanvas = document.getElementById("heightChart");
     const heightBins = appUtils.heightBins;
-    const heightChart =  new Chart(heightCanvas.getContext("2d"), {
+    return new Chart(heightCanvas.getContext("2d"), {
       type: "horizontalBar",
       data: {
         labels: heightBins.map(function (element) { return element.label }),
         datasets: [
           {
-            label: "Gebouwen met deze hoogte",
+            label: "Buildings with this height",
             backgroundColor: heightBins.map(function (element) { return element.color }),
             data: [0, 0, 0, 0, 0, 0, 0, 0]
           }
@@ -109,7 +67,7 @@ define(["app/config", "app/utils", "app/statistics", "app/main"], function (conf
         },
         title: {
           display: true,
-          text: "Aantal gebouwen op basis van hoogte"
+          text: "Number of buildings by height"
         },
         scales: {
           xAxes: [
@@ -129,43 +87,6 @@ define(["app/config", "app/utils", "app/statistics", "app/main"], function (conf
         }
       }
     });
-
-    heightCanvas.onclick = function(evt)
-
-    {   
-      if (click_height == false){
-
-        click_height = true;
-        var activePoints = heightChart.getElementsAtEvent(evt);
-        var clickedElementindex = activePoints[0]["_index"];
-        var label = heightChart.data.labels[clickedElementindex];
-        var heights = label.split(" ");
-        
-        
-        if (heights[2] != null) {
-          var start_height = heights[0];
-          var end_height = heights[2].substring(0, heights[2].lastIndexOf("m"));
-
-          def_expression_height = "AND MAX_hoogte >= " + start_height + " AND MAX_hoogte < " + end_height + " ";
-        }
-
-        else {
-          var height = heights[1].substring(0, heights[1].lastIndexOf("m"));
-          def_expression_height = "AND MAX_hoogte " + heights[0] + " " + height + " ";
-        }
-          
-        
-      }
-        
-      else{
-        click_height = false;
-        def_expression_height = "AND 1=1 ";
-      }
-      
-
-      defExpression(def_expression_date,def_expression_height,def_expression_usage);  
-    }
-    return heightChart;
   }
 
   function createUsageChart() {
@@ -173,7 +94,7 @@ define(["app/config", "app/utils", "app/statistics", "app/main"], function (conf
     const labels = config.usageValues.map(function (element) {
       return element.label;
     })
-    labels.push("Andere");
+    labels.push("Other");
 
     const backgroundColor = config.usageValues.map(function (element) {
       return element.color;
@@ -197,43 +118,14 @@ define(["app/config", "app/utils", "app/statistics", "app/main"], function (conf
         responsive: false,
         cutoutPercentage: 35,
         legend: {
-          display: false,
           position: "bottom"
         },
         title: {
           display: true,
-          text: "Gebruiksfunctie gebouw"
+          text: "Building usage"
         }
       }
     });
-
-    usageCanvas.onclick = function(evt)
-
-    {   
-      if (click_usage == false){
-
-        click_usage = true;
-        var activePoints = usageChart.getElementsAtEvent(evt);
-        var clickedElementindex = activePoints[0]["_index"];
-        var label = usageChart.data.labels[clickedElementindex];
-
-        if (label == "Other"){
-          def_expression_usage = "AND Gebruiksfunctie IS NULL ";
-        }
-        else{
-          def_expression_usage = "AND Gebruiksfunctie LIKE '" + label.toLowerCase() + "'";
-        }
-      }
-
-      else {
-        click_usage = false;
-        def_expression_usage = "AND 1=1";
-      }
-      
-
-      defExpression(def_expression_date,def_expression_height,def_expression_usage);  
-    }
-
     return usageChart;
   }
 
