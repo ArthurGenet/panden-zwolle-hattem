ef10362b0aac42ad3d8f8b6bbf46925530a15245
Arthur Genet <arthurgenet@yahoo.fr>
2020-07-02 16:38:00 +0200

    test88

--- app/main.js
+++ app/main.js
@@ -89,7 +89,7 @@ define([
             bdgLayer = layer;
             bdgLayer.popupTemplate = {
               content: `Dit gebouw is {${config.heightField}}m lang, gebouwd in
-              {${config.yearField}} en het heeft een {${config.usageField[1]}}.`
+              {${config.yearField}} en het heeft een {${config.usageField[1][0]}}.`
             };
             console.log("ok2");
             //console.log(config.heightField);
