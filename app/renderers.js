59d2dc1abe60275edfa906c305cf450eb1c6ccb5
Arthur Genet <arthurgenet@yahoo.fr>
2020-07-07 13:52:45 +0200

    test243

--- app/renderers.js
+++ app/renderers.js
@@ -61,6 +61,7 @@ define([
         }
       }
     },
+
     applyAreaRenderer: function (layer) {
 
       classBreakInfos = config.areaClasses.map(function (element) {
@@ -96,6 +97,45 @@ define([
         classBreakInfos: classBreakInfos
       }
     },
+
+    applyUsageRenderer: function(layer){
+    	layer.renderer =
+    	{
+  type: "unique-value",  // autocasts as new UniqueValueRenderer()
+  field: "is_bijeenk",
+  field2: "is_winkel",
+  defaultSymbol: { type: "simple-fill" },  // autocasts as new SimpleFillSymbol()
+  uniqueValueInfos: [{
+    // All features with value of "North" will be blue
+    value: "1,0",
+    symbol: {
+      type: "simple-fill",  // autocasts as new SimpleFillSymbol()
+      color: "blue"
+    }
+  }, {
+    // All features with value of "East" will be green
+    value: "0,1",
+    symbol: {
+      type: "simple-fill",  // autocasts as new SimpleFillSymbol()
+      color: "green"
+    }
+  }, {
+    // All features with value of "South" will be red
+    value: "South",
+    symbol: {
+      type: "simple-fill",  // autocasts as new SimpleFillSymbol()
+      color: "red"
+    }
+  }, {
+    // All features with value of "West" will be yellow
+    value: "West",
+    symbol: {
+      type: "simple-fill",  // autocasts as new SimpleFillSymbol()
+      color: "yellow"
+    }
+  }]
+}; 
+    
 
     applyOriginalTexture: function (layer) {
       layer.renderer = null;
